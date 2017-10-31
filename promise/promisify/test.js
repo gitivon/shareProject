27d89dc65324b5c9d66b0bfa9c19ec51c5423df5
华清峰 <80506313@qq.com>
2017-10-31 16:55:31 +0800

    promisify 实现

--- promise/promisify/test.js
+++ promise/promisify/test.js
@@ -2,10 +2,12 @@ import promisify from './promisify'
 import fs from 'fs'
 import path from 'path'
 
+const readdir = promisify(fs.readdir)
+
 (async () => {
   try{
     // fs
-    const files = await promisify(fs.readdir)(path.resolve(__dirname))
+    const files = await readdir(path.resolve(__dirname))
     console.log(files)
 
   } catch(e){
