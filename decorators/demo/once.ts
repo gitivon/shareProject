c328f0672ca2a01d5c2da340f902236be6689287
ivonhua <ivonhua@vipabc.com>
2018-04-12 18:31:28 +0800

    edit demo

--- decorators/demo/once.ts
+++ decorators/demo/once.ts
@@ -9,3 +9,10 @@ class A {
   }
 
 }
+
+const a = new A();
+a.demoMethod(); // console and cache result
+a.demoMethod(); // will not console, but will get result
+
+const b = new A();
+b.demoMethod(); // console and cache result
